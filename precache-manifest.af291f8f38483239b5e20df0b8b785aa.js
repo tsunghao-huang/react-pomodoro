64d28cce45277623ec4d7fa50d40b36c27a02fbe
Tsunghao Huang <huang.tsunghao@gmail.com>
2020-07-05 12:08:15 +0200

    Updates

---
+++ precache-manifest.af291f8f38483239b5e20df0b8b785aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2623815c64bc1a0388305ee6a4134b5",
+    "url": "/react-pomodoro/index.html"
+  },
+  {
+    "revision": "4b7547ad5ef2c340d51a",
+    "url": "/react-pomodoro/static/css/main.1b00add9.chunk.css"
+  },
+  {
+    "revision": "160f7706a348bbe62554",
+    "url": "/react-pomodoro/static/js/2.94f1bd64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pomodoro/static/js/2.94f1bd64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b7547ad5ef2c340d51a",
+    "url": "/react-pomodoro/static/js/main.1e61c74a.chunk.js"
+  },
+  {
+    "revision": "7bccaee7c4903395c012",
+    "url": "/react-pomodoro/static/js/runtime-main.44319084.js"
+  }
+]);
