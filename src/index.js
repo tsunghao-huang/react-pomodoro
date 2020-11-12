8a69b8d4d5f39fe60e56c2fc01cb5886e74b0eeb
Tsunghao Huang <huang.tsunghao@gmail.com>
2020-11-12 18:21:38 +0100

    adjust initial css

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import './index.css';
 import Pomodoro from './components/Pomodoro';
 import TodoApp from './components/TodoApp';
 
+const DATA = [
+    { id: "todo-0", name: "Eat", completed: true },
+    { id: "todo-1", name: "Sleep", completed: false },
+    { id: "todo-2", name: "Repeat", completed: false }
+];
+
 function App() {
     return (
         <div>
             <Pomodoro />
-            <TodoApp />
+            <TodoApp tasks={DATA} />
         </div>
 
     )
