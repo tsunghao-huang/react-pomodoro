494fad44195e4842ca87e5217728c5ab0ae2f08f
Tsunghao Huang <huang.tsunghao@gmail.com>
2020-11-13 20:51:19 +0100

    finished main functionalities

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,33 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./TodoApp.css";
 import Form from "./TodoApp-sub/Form";
 import FilterButton from "./TodoApp-sub/FilterButton";
 import Todo from "./TodoApp-sub/Todo";
+import { nanoid } from "nanoid";
 
-
+const FILTER_MAP = {
+    All: () => true,
+    Active: task => !task.completed,
+    Completed: task => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 export default function TodoApp(props) {
 
-    const taskList = props.tasks.map(task => (
-        <Todo
-            id={task.id}
-            name={task.name}
-            completed={task.completed}
-            key={task.id}
+    const [tasks, setTasks] = useState(props.tasks);
+    const [filter, setFilter] = useState('All');
+
+    function addTask(name) {
+        const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+        setTasks([...tasks, newTask]);
+    }
+
+    function toggleTaskCompleted(id) {
+        const updatedTasks = tasks.map(task => {
+            // if this task has the same ID as the edited task
+            if (id === task.id) {
+                // use object spread to make a new obj
+                // whose `completed` prop has been inverted
+                return { ...task, completed: !task.completed }
+            }
+            return task;
+        });
+        setTasks(updatedTasks);
+    }
+
+    function deleteTask(id) {
+        const remainingTasks = tasks.filter(task => id !== task.id);
+        setTasks(remainingTasks);
+    }
+
+    function editTask(id, newName) {
+        const editedTaskList = tasks.map(task => {
+            // if this task has the same ID as the edited task
+            if (id === task.id) {
+                //
+                return { ...task, name: newName };
+            }
+            return task;
+        });
+        setTasks(editedTaskList);
+    }
+
+    const taskList = tasks
+        .filter(FILTER_MAP[filter])
+        .map(task => (
+            <Todo
+                id={task.id}
+                name={task.name}
+                completed={task.completed}
+                key={task.id}
+                toggleTaskCompleted={toggleTaskCompleted}
+                deleteTask={deleteTask}
+                editTask={editTask}
+            />
+        ));
+
+    const filterList = FILTER_NAMES.map(name => (
+        <FilterButton
+            key={name}
+            name={name}
+            isPressed={name === filter}
+            setFilter={setFilter}
         />
     ));
+
+    const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+    const headingText = `${taskList.length} ${tasksNoun} remaining`
+
     return (
         <div className="todoapp stack-large">
             <h1>Todos</h1>
-            <Form />
+            <Form addTask={addTask} />
             <div className="filters btn-group stack-exception">
-                <FilterButton />
-                <FilterButton />
-                <FilterButton />
+                {filterList}
             </div>
             <h2 id="list-heading">
-                3 tasks remaining
-        </h2>
+                {headingText}
+            </h2>
             <ul
                 role="list"
                 className="todo-list stack-large stack-exception"
