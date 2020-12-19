e26cbfb2beea1ea10ae78e8eaade38186443205f
Tsunghao Huang <huang.tsunghao@gmail.com>
2020-12-19 11:24:51 +0100

    Updates

--- precache-manifest.015d841e91b9d96ae32909ce3507402c.js
+++ precache-manifest.015d841e91b9d96ae32909ce3507402c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6a7a7cb1272007c459524a50dd40a15a",
+    "revision": "4482fed2eb77af2bdcbcd6a6e6409fd3",
     "url": "/react-pomodoro/index.html"
   },
   {
-    "revision": "f6538c53a7a61604639e",
-    "url": "/react-pomodoro/static/css/main.11d0c56a.chunk.css"
+    "revision": "108ffa55d7d65ec66c86",
+    "url": "/react-pomodoro/static/css/main.b8e270fb.chunk.css"
   },
   {
     "revision": "d675aa478c33e1cd631f",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-pomodoro/static/js/2.1a8116b1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f6538c53a7a61604639e",
+    "revision": "108ffa55d7d65ec66c86",
     "url": "/react-pomodoro/static/js/main.feee78e6.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-pomodoro/precache-manifest.fb3e6aa3447998583e4295c685dc4310.js"
+  "/react-pomodoro/precache-manifest.015d841e91b9d96ae32909ce3507402c.js"
 );
 
 self.addEventListener('message', (event) => {
